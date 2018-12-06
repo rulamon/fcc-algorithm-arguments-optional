6d305f2312522e13401ec555a0ff9b38d3cd1144
rulamon <ruben.lamon@gmail.com>
2018-12-06 21:39:40 +0100

    added comments

--- 1.js
+++ 1.js
@@ -1,12 +1,15 @@
 function addTogether(x) {
+	//check if first argument isn't a number, if this is the case, return undefined
 	if (typeof arguments[0] !== "number") {
 		return undefined;
+		//check if there's a second argument, and if this is a number, add it to the first argument.
+		//otherwise, return undefined
 	} else if (arguments.length > 1 ) {
 		return typeof arguments[1] === "number" ? arguments[0] + arguments[1] : undefined;
-	}
-	return function sumTwo(y) {
-		return typeof y === "number" ? x + y : undefined;
+	} else {
+		//curried function for seperate argument
+		return y => typeof y === "number" ? x + y : undefined
 	};
 }
 
-console.log(addTogether("http://bit.ly/IqT6zt"));
+console.log(addTogether(3)(4));
